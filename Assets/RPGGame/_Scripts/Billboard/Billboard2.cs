2d29caea75d792872288641061ba7fc9008fd0af
Dophian <169108628+Dophian@users.noreply.github.com>
2024-06-20 15:13:06 +0900

    Initial commit

---
+++ Assets/RPGGame/_Scripts/Billboard/Billboard2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard2 : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+    void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(_mainCamera.transform.forward);
+    }
+}
